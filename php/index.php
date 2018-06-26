70a5a083ad38866bfb413e1a6df73eac639adbb7
took13 <took13@hotmail.com>
2018-06-26 23:19:26 +0700

    Update index.php

--- php/index.php
+++ php/index.php
@@ -7,19 +7,12 @@ $bot = new BOT_API($channelSecret, $access_token);
 if (!empty($bot->isEvents)) {
 $reply_token = $bot->replyToken;
 $user_message = $bot->message;
-$res = $bot->getProfile($bot->userId);
-if ($res->isSucceeded()){
-$profile = $res->getJSONDecodedBody();
-	
-	//$profile = $bot->userProfile($reply_token);
+//$profile = $bot->userProfile($reply_token);
     $bot->replyMessageNew($reply_token, $profile['displayName']);
     if ($bot->isSuccess()) {
         echo 'Succeeded!';
         exit();
     }	
-	
-{
-
     // Failed
     echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
     exit();
