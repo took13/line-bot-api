e8ccd3bbad54ce462757f4114e3b6be9521bc97c
took13 <took13@hotmail.com>
2018-06-27 00:36:27 +0700

    Update index.php

--- php/index.php
+++ php/index.php
@@ -3,23 +3,25 @@ include ('line-bot.php');
 $channelSecret = 'd831e7e4bd15e966b258ed6757a9cb99';
 $access_token  = 'XCBJd9Dqc1kfB/VLFnVuEWJ/AjkLXgLdLnSbGuqXeMDwvpfV/XF3iHVtZNso7Jq4onCK9fmbHQ8MZsgrU7ZjCS1+tQk+STz2/0gm4Juod3sG7lRtRmv6bRGtGPAZABQqH3593dxp7W+ladz/KDMR0gdB04t89/1O/w1cDnyilFU=';
 $bot = new BOT_API($channelSecret, $access_token);
-	
+$reply_message = null;
+$message = $bot->message;
+
 if (!empty($bot->isEvents)) {
-	
+	//Get user profile
 	$res = $bot->getProfile($bot->userId);
 	if ($res->isSucceeded()) {
 	    $profile = $res->getJSONDecodedBody();
 	    $displayName = $profile['displayName'];
 	    $statusMessage = $profile['statusMessage'];
 	    $pictureUrl = $profile['pictureUrl'];
-	    $message = "Hello " . $displayName . ", how may I assist you today?";
-	    $bot->replyMessageNew($bot->replyToken, $message);
-	    if ($bot->isSuccess()) {
-		echo 'Succeeded!';
-		exit();
-	    }			
 	}
-
+	
+    $reply_message = "Hello " . $displayName . ", how may I assist you today?";
+    $bot->replyMessageNew($bot->replyToken, $message);
+    if ($bot->isSuccess()) {
+	echo 'Succeeded!';
+	exit();
+    }	
     // Failed
     echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
     exit();
