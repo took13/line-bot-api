4cc310c26d2c7a71ec15d1465c45d9e64ae47924
took13 <took13@hotmail.com>
2018-06-27 00:47:48 +0700

    Update index.php

--- php/index.php
+++ php/index.php
@@ -18,7 +18,7 @@ if (!empty($bot->isEvents)) {
 	if (strpos($text, '#gspp1') !== false){
 		$reply_message = 'unit1 : 35.8 mw' . '\n' . 'unit 2 : 33.6 mw';
 	} else if (strpos($text, '#ge') !== false){
-		$reply_message = '1a : 22.5 mw' . 'n' . '1b : 23.4 mw';
+		$reply_message = '1a : 22.5 mw' . '\n' . '1b : 23.4 mw';
 	} else {
 		$reply_message = "Hello " . $displayName . ", how may I assist you today?";		
 	}
