3661e33eace495d6212ad281b1510b5d33d2edc1
took13 <took13@hotmail.com>
2018-06-27 00:38:12 +0700

    Update index.php

--- php/index.php
+++ php/index.php
@@ -3,8 +3,7 @@ include ('line-bot.php');
 $channelSecret = 'd831e7e4bd15e966b258ed6757a9cb99';
 $access_token  = 'XCBJd9Dqc1kfB/VLFnVuEWJ/AjkLXgLdLnSbGuqXeMDwvpfV/XF3iHVtZNso7Jq4onCK9fmbHQ8MZsgrU7ZjCS1+tQk+STz2/0gm4Juod3sG7lRtRmv6bRGtGPAZABQqH3593dxp7W+ladz/KDMR0gdB04t89/1O/w1cDnyilFU=';
 $bot = new BOT_API($channelSecret, $access_token);
-$reply_message = null;
-$message = $bot->message;
+$text = $bot->message;
 
 if (!empty($bot->isEvents)) {
 	//Get user profile
@@ -17,7 +16,7 @@ if (!empty($bot->isEvents)) {
 	}
 	
     $reply_message = "Hello " . $displayName . ", how may I assist you today?";
-    $bot->replyMessageNew($bot->replyToken, $message);
+    $bot->replyMessageNew($bot->replyToken, $reply_message);
     if ($bot->isSuccess()) {
 	echo 'Succeeded!';
 	exit();
