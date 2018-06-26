bc3e65666a091b8a17289eef01d795c000e9f47a
took13 <took13@hotmail.com>
2018-06-26 23:34:54 +0700

    Update index.php

--- php/index.php
+++ php/index.php
@@ -1,5 +1,6 @@
 <?php
 include ('line-bot.php');
+include ('vendor/autoload.php');
 $channelSecret = 'd831e7e4bd15e966b258ed6757a9cb99';
 $access_token  = 'XCBJd9Dqc1kfB/VLFnVuEWJ/AjkLXgLdLnSbGuqXeMDwvpfV/XF3iHVtZNso7Jq4onCK9fmbHQ8MZsgrU7ZjCS1+tQk+STz2/0gm4Juod3sG7lRtRmv6bRGtGPAZABQqH3593dxp7W+ladz/KDMR0gdB04t89/1O/w1cDnyilFU=';
 $bot = new BOT_API($channelSecret, $access_token);
